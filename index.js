b1cd08dafaa0e9fb11e1d9df5c6fd28493b4f164
jarvi652 <jarviskatana@gmail.com>
2020-12-24 13:48:02 +0300

    'final'

--- index.js
+++ index.js
@@ -1,19 +1,79 @@
-var rock = document.querySelector("#rock").addEventListener('click',game)
-var paper = document.querySelector("#paper").addEventListener('click',game)
-var scissors = document.querySelector("#scissors").addEventListener('click',game)
+playerPlay = function(){
+    var selection={
+        rock:"#rock",
+        paper:"#paper",
+        scissors:"#scissors",
+          
+    }
+    return{
+       
+        selct: function(){
+            return selection
+        }      
 
-function computerPlay() {
-    let choices=['rock','paper','scissors']
-    return choices[Math.floor(Math.random() * choices.length)]
-    
-}
-function playRound(playerSelection,computerSelection) {
-    let computerSelection = computerPlay()
-    let result = ''
-    if(playerSelection=='rock' && computerSelection=='rock'){
+
+
+    }
+}()
+computerPlay = function(){
+    list=['rock','paper','scissors']
+    return{
+        cselect: function(){
+            return list[Math.floor(Math.random()*list.length)]
+        }
         
     }
-}
-function game(){
+    
+}()
 
-}
+playRound = function (playerSelection,computerSelection) {
+    var eventMatters = function(){
+        var player =playerSelection.selct()
+        var Rock = document.querySelector(player.rock)
+        Rock.addEventListener("click",  function(){
+            if(computer() == "rock"){
+                alert('it is a tie')
+            }else if(computer()=="paper"){
+                alert('Computer wins')
+            }else if(computer() == "scissors"){
+                alert('you have won')
+            }
+        })
+        var Paper = document.querySelector(player.paper)
+        Paper.addEventListener("click",  function(){
+            if(computer() == "paper"){
+                alert('it is a tie')
+            }
+            else if(computer() == "rock"){
+                alert('you have won')
+            }
+            else if(computer()=="scissors"){
+                alert('Computer wins')
+            }
+        })
+        var Scissors = document.querySelector(player.scissors)
+        Scissors.addEventListener("click",  function(){
+            if(computer() == "scissors"){
+                alert('it is a tie')
+            }
+            else if(computer()=="rock"){
+                alert('Computer wins')
+            }
+            else if(computer() == "paper"){
+                alert('you have won')
+            }
+        })
+    }
+    var computer = function(){
+        return computerSelection.cselect()
+    }
+    
+    
+    return{
+        init:function(){
+            eventMatters()
+        }
+    }
+}(playerPlay,computerPlay)
+
+playRound.init()
